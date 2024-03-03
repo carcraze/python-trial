8f4ed5f9fdea65283f22bc3fb164634df19f9387
Alvindecoder <alvinnyota05@gmail.com>
2024-03-03 16:42:26 +0300

    my second code

---
+++ index.py
@@ -0,0 +1,10 @@
+print('what is your name? ')
+name = input()
+
+print ('what is your age? ')
+print = input()
+
+print ('what is your location? ')
+print = input()
+
+print('Hello' , name , 'you are' , age , 'years old and live in' ,location)
